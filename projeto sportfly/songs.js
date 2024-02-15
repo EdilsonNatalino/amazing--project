4a49082b92220c1511eb41ea7fd7778abd3e77a4
Edilson Natalino <138825738+EdilsonNatalino@users.noreply.github.com>
2024-02-14 21:08:38 -0300

    Add files via upload

---
+++ projeto sportfly/songs.js
@@ -0,0 +1,54 @@
+export default [
+    {
+        src: "./src/01 Torn.mp3", 
+        name: "Creed - Torn",
+    },
+    {
+        src: "./src/02 My Own Prison.mp3",
+        name: "Creed - My Own prision",
+    },
+    {
+        src: "./src/03 What's This Life For.mp3",
+        name: "Creed - What's This Life For",
+    },
+    {
+        src: "./src/04 One.mp3",
+        name: "Creed - One",
+    },
+    {
+        src: "./src/05 Are You Ready.mp3",
+        name: "Creed - Are You Ready",
+    },
+    {
+        src: "./src/06 Higher.mp3",
+        name: "Creed - Higher",
+    },
+    {
+        src: "./src/07 With Arms Wide Open.mp3", 
+        name: "Creed - With Arms Wide Open",
+    },
+    {
+        src: "./src/08 What If.mp3",
+        name: "Creed - What If",
+    },
+    {
+        src: "./src/09 One Last Breath.mp3",
+        name: "Creed - One Last Breath",
+    },
+    {
+        src: "./src/10 Don't Stop Dancing.mp3",
+        name: "Creed - Don't Stop Dancing",
+    },
+    {
+        src: "./src/11 Bullets.mp3",
+        name: "Creed - Bullets",
+    },
+    {
+        src: "./src/12 My Sacrifice.mp3",
+        name: "Creed - My Sacrifice",
+    },
+    {
+        src: "src/13 Weathered.mp3",
+        name: "Creed - Weathered",
+    },
+]
